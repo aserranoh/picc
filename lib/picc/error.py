19d47776502919e2a04f6629327dd92219236936
aserranoh <toni.serranoh@gmail.com>
2016-02-28 23:48:21 -0300

    Added colored diagnostic messages.

--- lib/picc/error.py
+++ lib/picc/error.py
@@ -33,31 +33,41 @@ __status__ = 'Development'
 
 PROGNAME = sys.argv[0]
 
+# Use colors if in a terminal
+if sys.stdout.isatty():
+    ERROR = '\033[1m\033[31merror\033[0m'
+    WARN = '\033[1m\033[33mwarning\033[0m'
+    FATAL = '\033[1m\033[31mfatal\033[0m'
+else:
+    ERROR = 'error'
+    WARN = 'warning'
+    FATAL = 'fatal'
+
 def fatal(msg):
     '''Prints a fatal error and exits.'''
-    print('{prog}: fatal: {msg}'.format(prog=PROGNAME, msg=msg),
+    print('{prog}: {fatal}: {msg}'.format(prog=PROGNAME, msg=msg, fatal=FATAL),
         file=sys.stderr)
     exit(1)
 
 def fatalf(filename, msg):
     '''Prints a fatal error occurred while treating a given file and exits.'''
-    print('{file}: fatal: {msg}'.format(file=filename, msg=msg),
+    print('{file}: {fatal}: {msg}'.format(file=filename, msg=msg, fatal=FATAL),
         file=sys.stderr)
     exit(1)
 
 def errorf(filename, msg):
     '''Prints an error message.'''
-    print('{file}: error: {msg}'.format(file=filename, msg=msg),
+    print('{file}: {error}: {msg}'.format(file=filename, msg=msg, error=ERROR),
         file=sys.stderr)
 
 def errorfa(filename, section, offset, msg):
     '''Prints an error message.'''
-    print('{file}:{section}+{offset:#x}: error: {msg}'.format(
-        file=filename, section=section, offset=offset, msg=msg),
+    print('{file}:{section}+{offset:#x}: {error}: {msg}'.format(
+        file=filename, section=section, offset=offset, msg=msg, error=ERROR),
         file=sys.stderr)
 
 def warnf(filename, msg):
     '''Prints a warning message.'''
-    print('{file}: warning: {msg}'.format(file=filename, msg=msg),
+    print('{file}: {warn}: {msg}'.format(file=filename, msg=msg, warn=WARN),
         file=sys.stderr)
 
